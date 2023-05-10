4197db019460b2f352e2e24e8c6c07e003ae82a2
Cyb3r HeX <102288409+cyb3rhex@users.noreply.github.com>
2023-05-10 11:54:15 +0300

    Update ParamHunter.py

--- ParamHunter.py
+++ ParamHunter.py
@@ -3,10 +3,26 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, parse_qs, urljoin
 
+
+def print_header():
+    header = r"""
+ .___                                __  __               .                
+ /   \   ___  .___    ___  , _ , _   |   |  ,   . , __   _/_     ___  .___ 
+ |,_-'  /   ` /   \  /   ` |' `|' `. |___|  |   | |'  `.  |    .'   ` /   \
+ |     |    | |   ' |    | |   |   | |   |  |   | |    |  |    |----' |   '
+ /     `.__/| /     `.__/| /   '   / /   /  `._/| /    |  \__/ `.___, /    
+                                                                           
+
+Developer: LSDeep
+"""
+    print(header)
+
+
 def extract_parameters(url):
     parsed_url = urlparse(url)
     query_params = parse_qs(parsed_url.query)
     return query_params
+
 
 def fetch_urls(url):
     try:
@@ -20,12 +36,12 @@ def fetch_urls(url):
     urls = [urljoin(url, link.get("href")) for link in soup.find_all("a")]
     return urls
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python ParamHunter.py [URL]")
-        sys.exit(1)
 
-    url = sys.argv[1]
+def main():
+    print_header()
+    print("Ex: https://www.website.com")
+    url = input("Please enter the URL to scan with (http/https): ")
+    
     all_urls = fetch_urls(url)
 
     for u in all_urls:
@@ -36,3 +52,7 @@ if __name__ == "__main__":
             for key, value in parameters.items():
                 print(f"{key}: {value[0]}")
             print()
+
+
+if __name__ == "__main__":
+    main()
